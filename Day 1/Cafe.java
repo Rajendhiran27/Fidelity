f405f3b5d6734d1780d73f1b21b100042a4360bb
Rajendhiran27 <81033046+Rajendhiran27@users.noreply.github.com>
2024-09-25 12:35:44 +0530

    Add files via upload

---
+++ Day 1/Cafe.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Cafe{
+    public static void main(String[] args){
+        int pizp=200;
+        int pufp=40;
+        int pep=120;
+        int npiz=5;
+        int npufs=6;
+        int npep=2;
+        int tpiz=npiz*pizp;
+        int tpup=npufs*pufp;
+        int tpep=npep*pep;
+        System.out.println(tpiz);
+        System.out.println(tpup);
+        System.out.println(tpep);
+    }
+}
